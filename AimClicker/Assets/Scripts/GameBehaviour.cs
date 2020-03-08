0a8f72c22fc805fc0c699d5dbe2c3b4ff742b5c6
Lev Khakimov <55328440+DeviJoe@users.noreply.github.com>
2020-03-08 23:23:15 +0300

    Начата работа с Canvas и выводом текста на него

---
+++ AimClicker/Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class IntParamEvent : UnityEvent<int>
+{
+
+}
+
+public class GameBehaviour  : MonoBehaviour
+{
+    private int _score;
+    [SerializeField]
+    private IntParamEvent _changeScore;
+
+    public void AddScore(int score)
+    {
+        _score += score;
+        _changeScore?.Invoke(_score);
+    }
+}
---
+++ AimClicker/Assets/Scripts/GameOverWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void SetScore(int score)
+    {
+        _scoreText.text = $"Total score: {score}";
+    }
+
+    public void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
